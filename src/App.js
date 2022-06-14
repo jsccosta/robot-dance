8204fefa9dd8f2e59c847efefcbe805610cd79d5
jsccosta <juliocosta1@gmail.com>
2022-06-14 20:45:51 +0100

    move score elements to own file

--- src/App.js
+++ src/App.js
@@ -5,44 +5,11 @@ import Welcome from './components/Welcome';
 import TeamsGenerator from './components/TeamsGenerator';
 import RobotCard from './components/RobotCard';
 import DanceOff from './components/DanceOff';
+import Scores from './components/Scores';
 
 import { generateRobotTeam } from './utils';
 
 import { allRobots, danceOffs } from './mock';
-
-const Scores = () => {
-  const wins = danceOffs
-    .map((robot) => robot.winner.name)
-    .reduce((group, robot) => {
-      group[robot] = group[robot] ? ++group[robot] : 1;
-      return group;
-    }, {});
-
-  const orderedWins = Object.entries(wins).sort(function (a, b) {
-    return b[1] - a[1];
-  });
-
-  return (
-    <>
-      <div>Scores</div>
-      {orderedWins.map((score) => {
-        const robotName = score[0];
-        const wins = score[1];
-        return (
-          <div>
-            {robotName} - {wins}
-          </div>
-        );
-      })}
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-      <Link to="/teamsGenerator">
-        <button>Play again</button>
-      </Link>
-    </>
-  );
-};
 
 function App() {
   return (
