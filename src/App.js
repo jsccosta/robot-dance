1ad91c138b7eaac5d5641d471e4815ffcbe73ae3
jsccosta <juliocosta1@gmail.com>
2022-06-14 20:41:13 +0100

    move dance off elements to own file

--- src/App.js
+++ src/App.js
@@ -4,55 +4,11 @@ import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import TeamsGenerator from './components/TeamsGenerator';
 import RobotCard from './components/RobotCard';
+import DanceOff from './components/DanceOff';
 
 import { generateRobotTeam } from './utils';
 
 import { allRobots, danceOffs } from './mock';
-
-const DanceOff = () => {
-  const danceOffPairs = () => {
-    const teamOne = generateRobotTeam(5);
-    // team two should not include items from team one
-    const teamTwo = generateRobotTeam(5);
-
-    const dancePairs = teamOne.map((robot, idx) => {
-      return {
-        robot1: robot,
-        robot2: teamTwo[idx],
-      };
-    });
-
-    return dancePairs;
-  };
-
-  const pairs = danceOffPairs();
-
-  return (
-    <>
-      <div className="faceOffContainer">
-        {pairs.map((item) => (
-          <>
-            <div className="versusCard">
-              <div>{<RobotCard robot={item.robot1} />}</div>
-              Vs
-              <div>{<RobotCard robot={item.robot2} />}</div>
-            </div>
-          </>
-        ))}
-      </div>
-
-      <div>Dance Off</div>
-      {/* show only before game kickoff */}
-      <Link to="/scores">
-        <button>View scores</button>
-      </Link>
-      {/* show when dance off is done */}
-      <Link to="/teamsGenerator">
-        <button>Play again</button>
-      </Link>
-    </>
-  );
-};
 
 const Scores = () => {
   const wins = danceOffs
