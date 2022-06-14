0f8aca5a61086108a489aa5a95371c900d88b8b8
jsccosta <juliocosta1@gmail.com>
2022-06-15 00:28:34 +0100

    robot card adjustments

--- src/components/RobotCard.js
+++ src/components/RobotCard.js
@@ -1,18 +1,32 @@
+const getBackgroundColor = (isWinner) => {
+  if (isWinner === undefined) {
+    return '#6d5656';
+  }
+  return isWinner ? '#0b5b26' : '#66090f';
+};
+
 const RobotCard = ({ robot }) => {
-  const { name, powermove, experience, outOfOrder, avatar, winner } = robot;
+  const { name, powermove, experience, avatar, winner } = robot;
+
+  const isWinner = winner === true;
 
   return (
     <div
       className="robotCard box"
       style={{
-        background:
-          winner !== undefined ? (winner === true ? 'green' : 'red') : '',
+        background: getBackgroundColor(winner),
       }}
     >
-      <div>{name}</div>
-      <div>{powermove}</div>
-      <div>{experience}</div>
-      <div>{outOfOrder}</div>
+      <div className="robotProperties">
+        <p>
+          Name: <b>{name}</b>
+        </p>
+        <p>Power move:{powermove}</p>
+      </div>
+      <p>
+        Exp: <b>{experience}</b>
+      </p>
+      <div>{isWinner && <b>Winner!!</b>}</div>
       <img alt={name} src={avatar} width="100px" />
     </div>
   );
